2d55946ef982ed8d57805550cfe2fa63b8b84291
Aidan Crane <aidancrane78@gmail.com>
2021-05-02 21:15:56 +0100

    Fixed problems with Auth, mainly imports.
    Fixed problems with Auth, mainly imports.

--- app/Http/Controllers/Socialite/GoogleOAuthController.php
+++ app/Http/Controllers/Socialite/GoogleOAuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Socialite;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use App\Jobs\SendRegistrationEmail;
+use App\Models\User;
 use Auth;
 
 class GoogleOAuthController extends Controller
@@ -50,6 +52,11 @@ class GoogleOAuthController extends Controller
           {
             // Account does exist locally but not with a google id.
             $own_user->google_user_id = $user->id;
+            if ($own_user->email_verified_at == "" || $own_user->email_verified_at == null)
+            {
+              // Email is verified now.
+              $own_user->email_verified_at = Carbon::now();
+            }
             $own_user->save();
             Auth::login($own_user);
           }
@@ -61,13 +68,14 @@ class GoogleOAuthController extends Controller
           // Account does not exist locally.
 
           // Almost unique username, doesn't work if another user has a name like aidancrane1.
-          $user_count = User::where('first_name', '=', $user->given_name)
-                  ->where('last_name', '=', $user->family_name)->count();
+          $user_count = User::where('first_name', '=', $user->given_name)->where('last_name', '=', $user->family_name)->count();
 
           $new_user = new User();
           $new_user->google_user_id = $user->id;
-          $new_user->first_name =  $user->given_name;
-          $new_user->last_name =  $user->family_name;
+          $new_user->first_name = $user->given_name;
+          $new_user->last_name = $user->family_name;
+          $new_user->friendly_name = $user->given_name;
+          $new_user->email_verified_at = Carbon::now();
           $new_user->email_address =  $user->getEmail();
           $new_user->save();
           Auth::login($new_user);
