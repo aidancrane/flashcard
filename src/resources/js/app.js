7cab34790990a254edfb815846d9ad172639b0c0
aidancrane <aidancrane78@gmail.com>
2024-01-21 14:33:34 +0000

    Removed datatables.net as a dependency

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,6 +1,6 @@
 import './bootstrap';
 
-import 'datatables.net';
-import 'datatables.net-bs4';
-import 'datatables.net-responsive';
-import 'datatables.net-responsive-bs4';
+// import 'datatables.net';
+// import 'datatables.net-bs4';
+// import 'datatables.net-responsive';
+// import 'datatables.net-responsive-bs4';
