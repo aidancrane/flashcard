a8964a9b839858265dfd78930816eff25a2def1a
Aidan Crane <aidancrane78@gmail.com>
2024-01-29 23:36:50 +0000

    Fixed issue with Can't use method return value in write context

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // Check if seeding the database is neccesary, and if not don't.
         // The command php artisan db:seed is run after every rebuild of dev, so this prevents the database being re-seeded completely over and over.
-        if (User::where('email_address', '=', 'aidancrane78@gmail.com')->count() = 0) {
+        if (User::where('email_address', '=', 'aidancrane78@gmail.com')->count() == 0) {
             $this->call(UserSeeder::class);
             $this->call(SetSeeder::class);
             $this->call(FlashcardSeeder::class);
